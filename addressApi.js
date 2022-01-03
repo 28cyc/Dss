289ddc5a9f31877e39ed762fe52e606d228edf7a
28cyc <by890228@gmail.com>
2022-01-04 04:26:38 +0800

    STUPID

--- addressApi.js
+++ addressApi.js
@@ -81,3 +81,21 @@ function findVillage(results) {
 	var villageId = ListVillage(countycode, towncode01).find(item => item.villageName == level4).villageId; //村里代碼(68000010001)
 	return { "countyname": countyname, "townname": level3, "villageId": villageId, "villageName": level4 };
 }
+
+function postData(url, data) {
+	// Default options are marked with *
+	return fetch(url, {
+		body: JSON.stringify(data), // must match 'Content-Type' header
+		cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+		credentials: 'same-origin', // include, same-origin, *omit
+		headers: {
+			'user-agent': 'Mozilla/4.0 MDN Example',
+			'content-type': 'application/json'
+		},
+		method: 'POST', // *GET, POST, PUT, DELETE, etc.
+		mode: 'cors', // no-cors, cors, *same-origin
+		redirect: 'follow', // manual, *follow, error
+		referrer: 'no-referrer', // *client, no-referrer
+	})
+		.then(response => response.json()) // 輸出成 json
+}
